650fd21bce40bfc5d1d513dc0db3e0190707bd82
Thanh <dtt6154@dtt6154gmail.t6154gmailcom>
2026-08-13 23:46:11 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,25 @@ const App = () => {
       <TodoNew 
       addNewTodo={addNewTodo}
       />
-      <TodoData 
+      {todoList.length > 0 ?
+        <TodoData 
       todoList={todoList}
       />
+      :
       <div className='todo-image'>
         <img src={reactLogo} className='logo'/>
       </div>
+}
+      {/* {todoList.length > 0 &&
+        <TodoData 
+      todoList={todoList}
+      />
+      }
+      {todoList.length === 0 &&
+      <div className='todo-image'>
+        <img src={reactLogo} className='logo'/>
+      </div>
+      } */}
     </div>
   )
 }
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -4,10 +4,10 @@ const TodoData = (props) => {
     <div className="todo-data">
       {todoList.map((item, index) => {//ham map se duyet qua tung phần tử trong todoList(giống vòng for nhưng khác là nó sẽ trả một gt mới)
         return(
-        <div className={`todo-item`} key={index.id}>//todo-item: css cho div va button ben duoi
+        <div className={`todo-item`} key={index.id}> 
           <div>{item.name}</div>
           <button>Delete</button>
-        </div>
+        </div>//todo-item: css cho div va button ben duoi
       )
       })}
     </div>
