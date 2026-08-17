397a049bda80a992318fea8900d86a2d9c5cfb5c
Thanh <dtt6154@dtt6154gmail.t6154gmailcom>
2026-08-17 20:44:57 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,11 @@ const App = () => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+const deleteTodo = (id) => {
+  const newTodo = todoList.filter(item => item.id !== id) // giu lai phan tu co id khac id ta chuyen vao
+  setTodoList(newTodo);
+}
+
 
   //{key:value}
   return (
@@ -35,6 +40,7 @@ const App = () => {
       {todoList.length > 0 ?
         <TodoData 
       todoList={todoList}
+      deleteTodo={deleteTodo}
       />
       :
       <div className='todo-image'>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,12 +1,19 @@
 const TodoData = (props) => {
-  const { todoList } = props;
+  const { todoList , deleteTodo} = props;
+
+  const handleClick = (id) => {
+    deleteTodo(id)
+  }
+
   return (
     <div className="todo-data">
       {todoList.map((item, index) => {//ham map se duyet qua tung phần tử trong todoList(giống vòng for nhưng khác là nó sẽ trả một gt mới)
         return(
-        <div className={`todo-item`} key={index.id}> 
+        <div className={`todo-item`} key={item.id}> 
           <div>{item.name}</div>
-          <button>Delete</button>
+          <button 
+              onClick={()=> handleClick(item.id)}
+              style={{ cursor: "pointer" }}>Delete</button>
         </div>//todo-item: css cho div va button ben duoi
       )
       })}
