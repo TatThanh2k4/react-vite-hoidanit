a4fb3ef84253f82fb521fc2bdf99125979bbf18a
Thanh <dtt6154@dtt6154gmail.t6154gmailcom>
2026-08-25 01:47:35 +0700

    video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <ul>
             {/*đường dẫn này đơn giản hơn đường dẫn tuyệt đối và tương đối học ở html vì nó liên quan đến đường dẫn route của trình duyệt*/}
-            <li><a className="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/products">Products</a></li>
+            <li><Link class="active" to="/">Home</Link></li>
+            <li><Link to="/users">Users</Link></li>
+            <li><Link to="/products">Products</Link></li>
         </ul>
     )
 }
