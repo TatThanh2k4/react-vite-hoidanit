da048ec024d8415454117a49830d176c0a2911d7
Thanh <dtt6154@dtt6154gmail.t6154gmailcom>
2026-08-24 13:04:11 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData'
 import TodoNew from './components/todo/TodoNew'
 import reactLogo from './assets/react.svg'
 import { useState } from 'react'
+import Header from "./components/layout/header";
+import Footer from './components/layout/footer'
 
 const App = () => {
  
@@ -32,7 +34,9 @@ const deleteTodo = (id) => {
 
   //{key:value}
   return (
-    <div className="todo-container">
+    <>
+      <Header />
+      <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew 
       addNewTodo={addNewTodo}
@@ -57,7 +61,9 @@ const deleteTodo = (id) => {
         <img src={reactLogo} className='logo'/>
       </div>
       } */}
-    </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,14 @@
+import './header.css';
+
+const Header = () => {
+    return (
+        <ul>
+            {/*đường dẫn này đơn giản hơn đường dẫn tuyệt đối và tương đối học ở html vì nó liên quan đến đường dẫn route của trình duyệt*/}
+            <li><a class="active" href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+            <li><a href="/products">Products</a></li>
+        </ul>
+    )
+}
+
+export default Header;
